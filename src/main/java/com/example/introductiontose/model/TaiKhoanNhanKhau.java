32e3e4aea55dc08f1af1a71893645cbce13226de
Hau Trinh Van <trinhvanhau2003@gmail.com>
2023-11-29 13:13:47 +0700

    change TaiKhoanNhanKhau and TaiKhoanNhanKhauDao

--- src/main/java/com/example/introductiontose/model/TaiKhoanNhanKhau.java
+++ src/main/java/com/example/introductiontose/model/TaiKhoanNhanKhau.java
@@ -6,8 +6,8 @@ package com.example.introductiontose.model;
 public class TaiKhoanNhanKhau {
     private String tentaikhoan;
     private String pass;
-    private int soCCCD;
-    
+    private String soCCCD;
+
     /**
      * Khởi tạo một đối tượng TaiKhoanNhanKhau mới với thông tin cung cấp.
      *
@@ -15,21 +15,21 @@ public class TaiKhoanNhanKhau {
      * @param tentaikhoan  Tên tài khoản.
      * @param pass         Mật khẩu.
      */
-    public TaiKhoanNhanKhau(int soCCCD, String tentaikhoan, String pass) {
+    public TaiKhoanNhanKhau(String soCCCD, String tentaikhoan, String pass) {
         this.soCCCD = soCCCD;
         this.tentaikhoan = tentaikhoan;
         this.pass = pass;
     }
-    
+
     /**
      * Lấy số CCCD của nhân khẩu.
      *
      * @return Số CCCD của nhân khẩu.
      */
-    public int getSoCCCD() {
+    public String getSoCCCD() {
         return soCCCD;
     }
-    
+
     /**
      * Lấy tên tài khoản.
      *
@@ -38,7 +38,7 @@ public class TaiKhoanNhanKhau {
     public String getTentaikhoan() {
         return tentaikhoan;
     }
-    
+
     /**
      * Lấy mật khẩu.
      *
@@ -47,7 +47,7 @@ public class TaiKhoanNhanKhau {
     public String getPass() {
         return pass;
     }
-    
+
     /**
      * Thiết lập mật khẩu mới.
      *
@@ -56,16 +56,16 @@ public class TaiKhoanNhanKhau {
     public void setPass(String pass) {
         this.pass = pass;
     }
-    
+
     /**
      * Thiết lập số CCCD của nhân khẩu.
      *
      * @param soCCCD Số CCCD của nhân khẩu.
      */
-    public void setSoCCCD(int soCCCD) {
+    public void setSoCCCD(String soCCCD) {
         this.soCCCD = soCCCD;
     }
-    
+
     /**
      * Thiết lập tên tài khoản mới.
      *
